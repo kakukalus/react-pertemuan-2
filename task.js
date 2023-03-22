134278f300ec5d09bfb8d4b0eeda80f66b988b6d
kakukalus <66096781+kakukalus@users.noreply.github.com>
2023-03-22 17:02:31 +0700

    update to modern js

--- task.js
+++ task.js
@@ -4,7 +4,7 @@
  * Object memiliki property: name, age, major.
  * Note: Ubah var menggunakan JavaScript Modern.
  */
-var users = [
+let users = [
   { name: 'Alfin', age: 22, major: 'Designer' },
   { name: 'Rehan', age: 54, major: 'Business Administration' },
   { name: 'Nurul', age: 22, major: 'Marketing' },
@@ -20,7 +20,7 @@ var users = [
  * Hint: Gunakan for/for-of.
  * Note: Ubah function menggunakan arrow function.
  */
- all = () => {
+ const all = () => {
   for (let i = 0; i < users.length; i++) {
     console.log(`Name: ${users[i].name} Age: ${users[i].age} Major: ${users[i].major}`);
   }
@@ -32,7 +32,7 @@ var users = [
  * Hint: Gunakan method push.
  * Note: Ubah function menggunakan arrow function.
  */
- store = (user) => {
+ const store = (user) => {
   users.push(user);
   all();
 }
@@ -43,7 +43,7 @@ var users = [
  * Hint: re-assign array.
  * Note: Ubah function menggunakan arrow function.
  */
- update = (index, user) => { 
+const update = (index, user) => { 
   users[index] = user;
   all();
 }
@@ -55,7 +55,7 @@ var users = [
  * Hint: Gunakan method splice.
  * Note: Ubah function menggunakan arrow function.
  */
- destroy =(index) => { 
+const destroy =(index) => { 
   users.splice(index,index);
   all();
 }
